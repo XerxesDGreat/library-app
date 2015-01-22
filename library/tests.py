8698977adbbf2fc8a7fe0c79ee7fb9e7712bbdcd
Josh Wickham <josh@i-josh.com>
2015-01-21 23:57:25 -0800

    added a test

--- library/tests.py
+++ library/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from models import Student
 
-# Create your tests here.
+class ModelTest(TestCase):
+    def test_student_full_name(self):
+        a = Student(first_name='asdf', last_name='qwer')
+        self.assertEqual('asdf qwer', a.full_name())
